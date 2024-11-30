740ce64ee4aeaea1f75eab7b128bfdfc8d3674d3
Mohammadhridoy <mdridoykha019@gmail.com>
2024-11-30 21:53:20 +0600

    order route

--- src/app/modules/orders/order.route.ts
+++ src/app/modules/orders/order.route.ts
@@ -5,6 +5,7 @@ import { orderControllers } from './order.controller';
 const router = express.Router()
 
 router.post('/create-order', orderControllers.createOrder)
+
 router.get('/revenue', orderControllers.getOrderRevenue)
 
 
