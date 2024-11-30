1bdae25da79ceca7231abde852c294704c4e4a46
Mohammadhridoy <mdridoykha019@gmail.com>
2024-11-30 21:38:30 +0600

    edit car route file

--- src/app/modules/cars/car.route.ts
+++ src/app/modules/cars/car.route.ts
@@ -5,9 +5,10 @@ import { carControllers } from './car.controller';
 const router = express.Router()
 
 router.post('/create-car', carControllers.createCar )
-router.get('/', carControllers.getAllCarsInfo)
 router.get('/:carId', carControllers.getOneCarInfo )
 router.put('/:carId', carControllers.updatedCarInfo )
 router.delete('/:carId', carControllers.deleteCarInfo)
+router.get('/', carControllers.getAllCarsInfo)
+
 
 export const carRoutes = router; 
